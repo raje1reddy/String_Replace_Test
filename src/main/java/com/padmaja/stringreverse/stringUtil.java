fee3dcc0db297d250c3cfdb003fb5c3d94cd244a
unknown <raje1reddy@gmail.com>
2019-08-22 17:55:05 +0100

    intial code for string replace method

---
+++ src/main/java/com/padmaja/stringreverse/stringUtil.java
@@ -0,0 +1,24 @@
+package com.padmaja.stringreverse;
+import java.lang.String;
+
+public class stringUtil {
+    public String reverse(String inputString) {
+        //convert string to array
+        char[] myarray= inputString.toCharArray();
+        for(int i=0,j = inputString.length()-1; i<j; i++,j--){
+            char temp = myarray[i];
+            myarray[i]= myarray[j];
+            myarray[j]= temp;
+        }
+        //convert array to string and assign the string to return it
+
+        //String reveseString =  myarray.toString();
+        //return new String(myarray);
+        //return  myarray.toString();
+       String reveseString = new String(myarray);
+       return reveseString;
+
+    }
+
+
+}
